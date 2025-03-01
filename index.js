6dae59160a1a65d7de1c4a8d131142cfd6d17ad3
Antonello <50622699+mccoy88f@users.noreply.github.com>
2025-03-01 19:33:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.json({limit: '5mb'}));
+app.use(express.urlencoded({limit: '5mb', extended: true}));
 
 
 app.post('/upload-playlist', (req, res) => {
