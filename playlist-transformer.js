37b5a9fdc61316f7efe7dc22647e7f0747235ed4
Antonello <50622699+mccoy88f@users.noreply.github.com>
2025-03-01 12:22:09 +0100

    Update playlist-transformer.js

--- playlist-transformer.js
+++ playlist-transformer.js
@@ -171,7 +171,6 @@ class PlaylistTransformer {
   
       // Se genres è vuoto, usa 'Other Channels'
       if (genres.length === 0) {
-          const genres = new Set(['Altri Canali']);
           genres = ['Altri Canali'];
       }
 
@@ -263,6 +262,7 @@ class PlaylistTransformer {
   async parseM3UContent(content, config) {
       const lines = content.split('\n');
       let currentChannel = null;
+      const genres = new Set(['Altri Canali']);
   
       let epgUrl = null;
       if (lines[0].includes('url-tvg=')) {
