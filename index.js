965b93d7782c524de61aa89992f1e1e868ab8a47
Antonello <50622699+mccoy88f@users.noreply.github.com>
2025-03-01 19:37:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,6 @@ const path = require('path');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json({limit: '5mb'}));
 app.use(express.urlencoded({limit: '5mb', extended: true}));
 
